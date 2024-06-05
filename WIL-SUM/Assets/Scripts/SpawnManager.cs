099ebc614104dec9547fd7f573c0fe11d7c0b7d8
Nkwemakaleng <nkwemakaleng@gmail.com>
2024-06-05 16:08:48 +0200

    Spawner
    Done ig

---
+++ WIL-SUM/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+  public Vector3 spawnerSize;
+
+  public Color gizmosColor;
+
+  public GameObject targetObject;
+  
+  public GameObject Object;
+
+  public int objectCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*while (CheckSpawn(objectCount))
+        {
+            StartCoroutine(spawnObject());
+        }*/
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (CheckSpawn(objectCount))
+        {
+            StartCoroutine(spawnObject());
+        }
+    }
+
+    public void addSpawnCount(int i)
+    {
+        objectCount = i;
+    }
+
+    bool CheckSpawn(int count)
+    {
+        if (count > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    IEnumerator spawnObject()
+    {
+        Vector3 gizmosPos = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y,0);
+        Vector3 SpawnPos = gizmosPos + new Vector3(Random.Range(-spawnerSize.x / 2, spawnerSize.x / 2),
+            Random.Range(-spawnerSize.y / 2, spawnerSize.y / 2), 12);
+        Instantiate(Object, SpawnPos, Quaternion.identity);
+        objectCount -= 1;
+        Debug.Log("Object Spawned");
+        yield return new WaitForSeconds(1f);
+    }
+
+   public void OnDrawGizmos()
+    {
+        if (targetObject != null)
+        {
+            Gizmos.color = gizmosColor;
+            Gizmos.DrawSphere(targetObject.transform.position, spawnerSize.x/2);
+            
+        }
+    }
+
+   private void unitSelection(GameObject Object)
+   {
+       
+   }
+}
